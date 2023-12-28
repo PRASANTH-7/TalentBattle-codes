77268f844e0dcf57a35fac83944c861bb64aff00
BHASKARAPRASATH SK <116350913+PRASANTH-7@users.noreply.github.com>
2023-12-28 18:50:35 +0530

    Create DAY-03.java

---
+++ DAY-03.java
@@ -0,0 +1,12 @@
+/*   Write a program to find ASCII values of a character */
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      
+      char ch=sc.next().charAt(0);
+      
+     System.out.println((int)ch);
+      
+  }
+}
